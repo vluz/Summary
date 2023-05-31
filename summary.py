709396e4bd8f7069afd04503888e09deca24bbb3
vluz <vluz@users.noreply.github.com>
2023-05-31 20:26:02 +0100

    Add files via upload

---
+++ summary.py
@@ -0,0 +1,22 @@
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Please provide the name of file as an argument.")
+    sys.exit()
+
+filename = sys.argv[1]
+
+try:
+    with open(filename, 'r') as file:
+        text = file.read()
+except FileNotFoundError:
+    print("File not found. Please check the file name and try again.")
+
+tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum")
+model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
+tokens = tokenizer(text, truncation=True, padding="longest", return_tensors="pt")
+summary = model.generate(**tokens)
+output = tokenizer.decode(summary[0])
+print(output)
